a112abcf73db03862a922c98986dac4dc94b631d
Peter McCracken <peter.mccracken@jadedpixel.com>
2014-03-20 21:50:21 +0000

    split up into two types of cassettes

--- lib/tcr/cassette.rb
+++ lib/tcr/cassette.rb
@@ -3,58 +3,83 @@ module TCR
     attr_reader :name
 
     def self.build(name)
-      new(name)
+      if cassette_exists?(name)
+        RecordedCassette.new(name)
+      else
+        RecordingCassette.new(name)
+      end
+    end
+
+    def self.filename(name)
+      "#{TCR.configuration.cassette_library_dir}/#{name}.json"
+    end
+
+    def self.cassette_exists?(name)
+      File.exists?(filename(name))
     end
 
     def initialize(name)
       @name = name
-      @recording = !File.exists?(filename)
-    end
-
-    def sessions
-      @sessions ||= if recording?
-        []
-      else
-        read_serialized_form
-      end
-    end
-
-    def recording?
-      @recording
-    end
-
-    def next_session
-      if recording?
-        Session.new([], true).tap do |session|
-          sessions << session
-        end
-      else
-        session = sessions.shift
-        raise NoMoreSessionsError unless session
-        Session.new(session, false)
-      end
-    end
-
-    def finish
-      if recording?
-        FileUtils.mkdir_p(File.dirname(filename))
-        File.open(filename, "w") { |f| f.write(serialized_form) }
-      end
     end
 
     protected
 
-    def read_serialized_form
-      raw = File.open(filename) { |f| f.read }
-      JSON.parse(raw)
-    end
-
-    def serialized_form
-      JSON.pretty_generate(sessions.map(&:as_json))
-    end
-
     def filename
-      "#{TCR.configuration.cassette_library_dir}/#{name}.json"
+      self.class.filename(name)
+    end
+
+    class RecordedCassette < Cassette
+      def sessions
+        @sessions ||= serialized_form
+      end
+
+      def recording?
+        false
+      end
+
+      def next_session
+        session = sessions.shift
+        raise NoMoreSessionsError unless session
+        Session.new(session, false)
+      end
+
+      def finish
+        # no-op
+      end
+
+      private
+
+      def serialized_form
+        raw = File.open(filename) { |f| f.read }
+        JSON.parse(raw)
+      end
+    end
+
+    class RecordingCassette < Cassette
+      def sessions
+        @sessions ||= []
+      end
+
+      def recording?
+        true
+      end
+
+      def next_session
+        Session.new([], true).tap do |session|
+          sessions << session
+        end
+      end
+
+      def finish
+        FileUtils.mkdir_p(File.dirname(filename))
+        File.open(filename, "w") { |f| f.write(serialized_form) }
+      end
+
+      private
+
+      def serialized_form
+        JSON.pretty_generate(sessions.map(&:as_json))
+      end
     end
 
     class Session
