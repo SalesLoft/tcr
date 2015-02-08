8d7c8118eda94cb95d18e3f51306554d6ac2ac0c
Richard Smiley <rsmiley@gmail.com>
2015-02-07 20:39:26 -0500

    configuration accessor for format

--- lib/tcr/configuration.rb
+++ lib/tcr/configuration.rb
@@ -1,6 +1,6 @@
 module TCR
   class Configuration
-    attr_accessor :cassette_library_dir, :hook_tcp_ports, :block_for_reads
+    attr_accessor :cassette_library_dir, :hook_tcp_ports, :block_for_reads, :format
 
     def initialize
       reset_defaults!
@@ -10,6 +10,7 @@ module TCR
       @cassette_library_dir = "fixtures/tcr_cassettes"
       @hook_tcp_ports = []
       @block_for_reads = false
+      @format = :json
     end
   end
 end
