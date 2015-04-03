9231b9b5e01e808aaba7777b9094b2c252535a6b
Ben Olive <sionide21@gmail.com>
2015-04-02 22:02:28 -0400

    Switch cassette form yahoo SMTP to linkedin SMTP
    Travis-CI only really supports IPv6 and yahoo doesn't have an IPv6 host
    for their mailserver. LinkedIn does so let's test with that

--- spec/tcr_spec.rb
+++ spec/tcr_spec.rb
@@ -221,12 +221,12 @@ describe TCR do
         TCR.use_cassette("test") do
           smtp = Net::SMTP.start("aspmx.l.google.com", 25)
           smtp.finish
-          smtp = Net::SMTP.start("mta6.am0.yahoodns.net", 25)
+          smtp = Net::SMTP.start("mail-c.linkedin.com", 25)
           smtp.finish
         end
         cassette_contents = File.open("test.json") { |f| f.read }
         cassette_contents.include?("google.com ESMTP").should == true
-        cassette_contents.include?("yahoo.com ESMTP").should == true
+        cassette_contents.include?("mail-c.linkedin.com ESMTP").should == true
       end
 
       it "plays back multiple sessions per cassette in order" do
